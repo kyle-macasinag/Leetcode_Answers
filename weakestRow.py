16770d819c12060d5ec4b999cdd15e4b0a0e1cbf
kyle.macasinag <kylenmac@verizon.net>
2022-08-16 10:21:49 -0400

    Corrected weakestRow.py

--- weakestRow.py
+++ weakestRow.py
@@ -12,7 +12,7 @@
 class Solution:
     def kWeakestRows(self, M: List[List[int]], K: int) -> List[int]:
         y, x = len(M), len(M[0])
-        vis, ans = [0] * y, []
+        vis, answer = [0] * y, []
         for j in range(x+1):
             for i in range(y):
                 if not vis[i] and (j == x or not M[i][j]):
